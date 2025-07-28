7f5455c7858c244ad4c866515354912911f81b96
mariaasghar123 <mariaasghar74@gmail.com>
2025-07-28 10:08:15 +0500

    upload fraont

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 
   (import.meta.env.MODE === "development" 
     ? "http://localhost:5000" 
-    : "https://verificationvisa.vercel.app");
+    : "https://verificationvisa.onrender.com");
 
  export default BASE_URL;
